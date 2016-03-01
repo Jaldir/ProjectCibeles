c8034d364461e82c3c342dfd5301251ef5af370d
David Leo <dangel.leo0@gmail.com>
2016-03-01 22:51:19 +0100

    Feat: Project
    Create the maven project and initial dependencies

---
+++ src/main/java/davidleo/ProjectCibeles/App.java
@@ -0,0 +1,13 @@
+package davidleo.ProjectCibeles;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
